751c92fa41a321e5d53a737fd473f58dd519485f
icyFatNinja <nyuan0301@gmail.com>
2024-08-30 22:24:49 +0200

    worked on calenderapp.cssD

--- src/Components/CalenderApp.jsx
+++ src/Components/CalenderApp.jsx
@@ -25,7 +25,7 @@ const CalenderApp = () => {
           <span>2</span>
           <span>3</span>
           <span>4</span>
-          <span>5</span>
+          <span className="current-day">5</span>
           <span>6</span>
           <span>7</span>
           <span>8</span>
